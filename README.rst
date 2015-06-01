1d7ebd5d6ece05cc6a72d35df797ea25af1721ca
Tim Pederick <pederick@gmail.com>
2015-06-01 17:43:59 +0800

    Added initial project files.

---
+++ README.rst
@@ -0,0 +1,15 @@
+=========================================
+PySpiro: A Python 3 wrapper for libspiro.
+=========================================
+
+PySpiro is a wrapper for ``libspiro``, a C library for Raph Levien's "Spiro"
+curve design tool.
+
+Licence
+=======
+PySpiro is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+See the file ``COPYING`` for the full licence terms.
---
+++ spiro/native.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""libspiro C interface."""
+
+# Copyright © 2015 Timothy Pederick.
+#
+# This file is part of PySpiro.
+#
+# PySpiro is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PySpiro is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PySpiro. If not, see <http://www.gnu.org/licenses/>.
+
+__all__ = []
+
+# Standard library imports
+import ctypes
